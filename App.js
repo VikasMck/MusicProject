e46e6f012a23139215ad2cec3d2f59bc7e8839c0
CoderEric04 <94086390+CoderEric04@users.noreply.github.com>
2023-11-29 12:11:43 +0000

    Changes

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const axios = require('axios');
 const { google } = require('googleapis');
+const base64 = require('base-64');  
 require('dotenv').config();
 
 const app = express();
@@ -21,18 +22,18 @@ app.listen(port, () => {
 
 async function getToken(spClientId, spClientSecret) {
   const authString = `${spClientId}:${spClientSecret}`;
-  const authBase64 = Buffer.from(authString, 'utf-8').toString('base64');
+  const authBase64 = base64.encode(authString);
 
   const url = 'https://accounts.spotify.com/api/token';
-  const headers = {
-    Authorization: `Basic ${authBase64}`,
+  const headers = { 
+    'Authorization': 'Basic ' + authBase64,
     'Content-Type': 'application/x-www-form-urlencoded',
   };
-  const data = { grant_type: 'client_credentials' };
+  const data = 'grant_type=client_credentials';
 
   try {
     const response = await axios.post(url, data, { headers });
-    const { access_token: token } = response.data;
+    const token = response.data.access_token;
     return token;
   } catch (error) {
     console.error(`Error getting token: ${error.message}`);
@@ -40,20 +41,26 @@ async function getToken(spClientId, spClientSecret) {
   }
 }
 
+
 async function getPlaylistItems(token, playlistId) {
   const url = `https://api.spotify.com/v1/playlists/${playlistId}/tracks`;
-  const headers = { Authorization: `Bearer ${token}` };
+  const headers = {
+    'Authorization': `Bearer ${token}`,
+  };
 
   try {
     const response = await axios.get(url, { headers });
-    const { items } = response.data;
+    const jsonResult = response.data;
 
-    if (items) {
-      const songs = items.map((item) => {
-        const { name: songName, artists } = item.track;
-        const artistNames = artists.map((artist) => artist.name).join(', ');
-        return `${songName} by ${artistNames}`;
+    if ('items' in jsonResult) {
+      const songs = jsonResult.items.map(item => {
+        const track = item.track;
+        const songName = track.name;
+        const artists = track.artists.map(artist => artist.name).join(', ');
+        const fullName = `${songName} by ${artists}`;
+        return fullName;
       });
+
       return songs;
     } else {
       console.log('No items found in the playlist.');
@@ -65,9 +72,13 @@ async function getPlaylistItems(token, playlistId) {
   }
 }
 
+
 async function getVideoIds(ytApiKey, videos) {
+  const apiServiceName = 'youtube';
+  const apiVersion = 'v3';
+
   const youtube = google.youtube({
-    version: 'v3',
+    version: apiVersion,
     auth: ytApiKey,
   });
 
@@ -82,107 +93,43 @@ async function getVideoIds(ytApiKey, videos) {
         maxResults: 1,
       });
 
-      const { items } = searchResponse.data;
+      const items = searchResponse.data.items;
 
-      if (items) {
+      if (items && items.length > 0) {
         const videoId = items[0].id.videoId;
         videoIds.push(videoId);
       } else {
         console.log(`No video found for query: ${video}`);
       }
     } catch (error) {
-      console.error(`Error getting video ID: ${error.message}`);
+      console.error(`Error searching for video: ${error.message}`);
     }
   }
 
   return videoIds;
 }
 
-async function createYTPlaylist(videoIds) {
-  const scopes = ['https://www.googleapis.com/auth/youtube', 'https://www.googleapis.com/auth/youtube.force-ssl'];
-
-  process.env.OAUTHLIB_INSECURE_TRANSPORT = '1';
-
-  const auth = new google.auth.GoogleAuth({
-    keyFile: 'yt.json',
-    scopes,
-  });
-
-  const youtube = google.youtube({
-    version: 'v3',
-    auth: await auth.getClient(),
-  });
-
-  try {
-    const createRequest = await youtube.playlists.insert({
-      part: 'snippet',
-      requestBody: {
-        snippet: {
-          title: 'Test Playlist',
-          description: 'This is a test',
-          privacyStatus: 'public',
-        },
-      },
-    });
-
-    const playlistId = createRequest.data.id;
-
-    for (const videoId of videoIds) {
-      await youtube.playlistItems.insert({
-        part: 'snippet',
-        requestBody: {
-          snippet: {
-            playlistId,
-            resourceId: {
-              kind: 'youtube#video',
-              videoId,
-            },
-          },
-        },
-      });
-    }
-
-    return createRequest.data;
-  } catch (error) {
-    console.error(`Error creating YouTube playlist: ${error.message}`);
-    return null;
-  }
-}
 
 app.post('/convert', async (req, res) => {
   try {
     const spClientId = process.env.SP_CLIENT_ID;
     const spClientSecret = process.env.SP_CLIENT_SECRET;
     const ytApiKey = process.env.YT_API_KEY;
-
-    const token = await getToken(spClientId, spClientSecret);
-
-    // Retrieve playlist ID from the request body
     const spPlaylistId = req.body.spPlaylistId;
 
-    // Check if spPlaylistId is present
-    if (!spPlaylistId) {
-      console.log('Invalid request: Missing spPlaylistId');
-      res.status(400).send('Invalid request: Missing spPlaylistId');
-      return;
-    }
+    const token = await getToken(spClientId, spClientSecret);
+    console.log(token);
 
-    const resultSP = await getPlaylistItems(token, spPlaylistId);
+    const songs = await getPlaylistItems(token, spPlaylistId);
+    console.log(songs);
 
+    const videoIds = await getVideoIds(ytApiKey, songs);
+    console.log(videoIds);
 
-    const videoIds = await getVideoIds(ytApiKey, resultSP);
-    const resultYT = await createYTPlaylist(videoIds);
-
-    if (resultYT) {
-      console.log('Playlist created successfully!');
-      console.log('Playlist ID:', resultYT.id);
-      res.send('Playlist created successfully! Playlist ID: ' + resultYT.id);
-    } else {
-      console.log('Failed to create YouTube playlist.');
-      res.status(500).send('Failed to create YouTube playlist.');
-    }
+    // Send the videoIds array to the frontend
+    res.json({ videoIds });
   } catch (error) {
-    console.error(`Error in button click handler: ${error.message}`);
-    res.status(500).send('An error occurred.');
+    console.error(`Error in /convert endpoint: ${error.message}`);
+    res.status(500).json({ error: 'Internal Server Error' });
   }
 });
